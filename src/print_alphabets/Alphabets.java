04e4506a5f9d2c8394b76ae9dccd3626d7bbf94e
Nazmul Hasan <nhrhandsome@gmail.com>
2017-12-16 21:01:14 -0500

    I have uploaded file for basic java

---
+++ src/print_alphabets/Alphabets.java
@@ -0,0 +1,19 @@
+//*****************************************************
+// Alphabets.java   Author: Nazmul Hasan
+// Demonstrates the basic structure of java application.
+//*******************************************************
+
+
+package print_alphabets;
+
+public class Alphabets {
+
+	public static void main(String[] args) {
+		char ch;
+		 
+	      for( ch = 'A' ; ch <= 'Z' ; ch++ )
+	         System.out.println(ch);
+		
+	}
+
+}
